17e7160c2014e44a1ef08d27a7d5c2dd7848219f
Amanda Trinity <amandatrinity16@gmail.com>
2024-12-29 21:32:38 -0300

    Deixando o css mais responsivo e trabalhando na lógica de JS

--- basico/ex1/ex1/javascript.js
+++ basico/ex1/ex1/javascript.js
@@ -1,17 +1,17 @@
 //Evento DOM
-var NomeCalculadoraIMC = window.document.getElementById('titulo')
-NomeCalculadoraIMC.addEventListener('click', clicar)
+var NomeCalculadoraIMC = window.document.getElementByClassName('title')
+NomeCalculadoraIMC.addEventListener('onclick', clicar)
 
 function clicar() {
     NomeCalculadoraIMC.innerText = 'Coloque todas as informações para saber seu Indíce de Massa Corporal!'
 }
 function calcular() {
-    let resultadoIMC = window.document.getElementById('calculo')
-    let altura = window.document.getElementsByClassName('input')[1]
-    var valorAltura = Number(altura.value)
-    let peso = window.document.getElementsByClassName('input')[2]
-    var valorPeso = Number(peso.value)
-    let calculoIMC = number((altura / (peso ** 2)).valueOf)
+    let resultadoIMC = window.document.getElementById('calculo').value
+    let altura = window.document.getElementsByClassName('input')[1].value
+    //var valorAltura = Number(altura.value)
+    let peso = window.document.getElementsByClassName('input')[2].value
+    //var valorPeso = Number(peso.value)
+    let calculoIMC = number((altura / (peso ** 2)).toFixed(2))
     resultadoIMC.innerHTML = 'Seu IMC é ${calculoIMC}'
     console.log(calculoIMC)
 }
