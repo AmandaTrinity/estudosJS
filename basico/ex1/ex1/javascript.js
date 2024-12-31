b0d8de11580595d2c751aae13cce5f2b1cc15900
Amanda Trinity <amandatrinity16@gmail.com>
2024-12-30 23:12:05 -0300

    Adicionei o hidden para remover no js e coloquei query selector em todas variáveis

--- basico/ex1/ex1/javascript.js
+++ basico/ex1/ex1/javascript.js
@@ -1,17 +1,18 @@
 //Evento DOM
-var NomeCalculadoraIMC = window.document.getElementByClassName('title')
-NomeCalculadoraIMC.addEventListener('onclick', clicar)
+const form = document.querySelector('div.form');
 
-function clicar() {
-    NomeCalculadoraIMC.innerText = 'Coloque todas as informações para saber seu Indíce de Massa Corporal!'
-}
-function calcular() {
-    let resultadoIMC = window.document.getElementById('calculo').value
-    let altura = window.document.getElementsByClassName('input')[1].value
-    //var valorAltura = Number(altura.value)
-    let peso = window.document.getElementsByClassName('input')[2].value
-    //var valorPeso = Number(peso.value)
-    let calculoIMC = number((altura / (peso ** 2)).toFixed(2))
-    resultadoIMC.innerHTML = 'Seu IMC é ${calculoIMC}'
+form.addEventListener('submit', function(dados) {
+    dados.preventDefault();
+
+    const  altura = window.document.querySelector('div#altura input').value;
+    const peso = window.document.querySelector('div#peso input').value;
+
+    const resultado = (peso / (altura**2)).toFixed(2)
+
+    const value = document.querySelector('div#value');
+    value.textContent = `Seu IMIC é ${resultado}`;
+
+    value.classList.remove('visually-hidden');
     console.log(calculoIMC)
-}
+
+});
