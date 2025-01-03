e06d892854c204fa8ad15d0f1548aacfa0c86d73
Amanda Trinity <amandatrinity16@gmail.com>
2025-01-02 21:00:41 -0300

    If e else para aparecer o resultado do imc

--- basico/ex1/javascript.js
+++ basico/ex1/javascript.js
@@ -1,18 +1,35 @@
 //Evento DOM
 const form = document.getElementById('form');
 
-form.addEventListener('submit', function(dados) {
+form.addEventListener('submit', function (dados) {
     dados.preventDefault();
-
-    const  altura = window.document.getElementById('altura').value;
+    
+    const altura = window.document.getElementById('altura').value;
     const peso = window.document.getElementById('peso').value;
 
-    const resultado = (peso / (altura**2)).toFixed(2)
+    const imc = number((peso / (altura ** 2)).toFixed(2))
 
-    const value = document.getElementById('value');
+    const value = document.getElementById('value')
     let description = '';
 
+    if (imc < 18, 5) {
+        description = 'magreza'
+    } else if (imc < 24, 9) {
+        description = 'Peso Normal'
+    } else if (resultado < 29,9){
+        description = 'Sobrepeso'
+    } else if (resultado < 34, 9) {
+        description = 'Obesidade Grau 1'
+    } else if (resultado < 39, 9) { 
+        description = 'Obesidade Grau 2'
+    } else if (resultado >= 40) {
+        description = 'Obesidade Grau 3'
+    };
+
+    
+    value.textContent = imc
+    document.getElementById('description').textContent = description;
 });
-document.getElementById("botaocalculo").addEventListener('click',() => {
-    document.getElementById('infos').hidden=false
+document.getElementById("botaocalculo").addEventListener('click', () => {
+    document.getElementById('infos').hidden = false
 });
