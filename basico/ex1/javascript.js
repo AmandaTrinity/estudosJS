979b5aa81c236c7abbfc65836a46a73bf4d2b1d1
Amanda Trinity <amandatrinity16@gmail.com>
2025-01-03 10:22:59 -0300

    feat(Calculadora-IMC) : adicionar formatação de nome e cores para classificação de IMC

--- basico/ex1/javascript.js
+++ basico/ex1/javascript.js
@@ -1,37 +1,54 @@
-   const botaoCalculo = document.getElementById('botaocalculo');
+const botaoCalculo = document.getElementById('botaocalculo');
 
-    botaoCalculo.addEventListener('click', function () {
-        const altura = parseFloat(document.getElementById('altura').value);
-        const peso = parseFloat(document.getElementById('peso').value);
-        const nome = document.getElementById('nome').value;
+botaoCalculo.addEventListener('click', function () {
+    const altura = parseFloat(document.getElementById('altura').value);
+    const peso = parseFloat(document.getElementById('peso').value);
+    const nome = document.getElementById('nome').value.toLowerCase();
+    const nomeFormatado = nome.split(' ');
 
-        if (isNaN(altura) || isNaN(peso)) {
-            alert('Por favor, insira valores válidos para altura e peso.');
-            return;
-        }
+    for (let i = 0; i < nomeFormatado.length; i++) {
+        nomeFormatado[i] = nomeFormatado[i][0].toUpperCase() + nomeFormatado[i].substr(1);
+    }
 
-        const imc = (peso / (altura ** 2)).toFixed(2);
-        let description = '';
+    const nomeFinal = nomeFormatado.join(' ');
 
-        if (imc < 18.5) {
-            description = 'Magreza';
-        } else if (imc < 24.9) { //obs: usp do ponto é melhor que vírgula
-            description = 'Peso Normal';
-        } else if (imc < 29.9) {
-            description = 'Sobrepeso';
-        } else if (imc < 34.9) {
-            description = 'Obesidade Grau 1';
-        } else if (imc < 39.9) {
-            description = 'Obesidade Grau 2';
-        } else {
-            description = 'Obesidade Grau 3';
-        }
+    if (isNaN(altura) || isNaN(peso)) {
+        alert('Por favor, insira valores válidos para altura e peso.');
+        return;
+    }
 
-        const valueElement = document.getElementById('value');
-        const descriptionElement = document.getElementById('description');
+    // Calculando o IMC e tratando como número
+    const imc = peso / (altura ** 2);
+    const imcFormatado = imc.toFixed(2).replace('.', ',');
 
-        valueElement.textContent = `Olá ${nome}, seu IMC é ${imc}`;
-        descriptionElement.textContent = `Classificação: ${description}`;
+    const valueElement = document.getElementById('value');
+    const descriptionElement = document.getElementById('description');
+    let classification = '';
 
-        document.getElementById('infos').hidden = false;
-    });
+    if (imc < 18.5) {
+        classification = 'Magreza';
+        valueElement.style.color = 'rgb(189, 1, 1)';
+    } else if (imc < 24.9) { 
+        classification = 'Peso Normal';
+        valueElement.style.color = 'rgb(97, 221, 15)';
+    } else if (imc < 29.9) {
+        classification = 'Sobrepeso';
+        valueElement.style.color = 'rgb(255, 217, 0)';
+    } else if (imc < 34.9) {
+        classification = 'Obesidade Grau 1';
+        valueElement.style.color = 'rgb(255, 94, 0)';
+    } else if (imc < 39.9) {
+        classification = 'Obesidade Grau 2';
+        valueElement.style.color = 'rgb(189, 5, 5)';
+    } else {
+        classification = 'Obesidade Grau 3';
+        valueElement.style.color = 'rgb(255, 0, 0)';
+    }
+
+    const nomeTextElement = document.getElementById('nomeTexto');
+    nomeTextElement.textContent = `Olá ${nomeFinal}, seu IMC é:`;
+    valueElement.textContent = ` ${imcFormatado}`;
+    descriptionElement.textContent = `Classificação: ${classification}`;
+
+    document.getElementById('infos').hidden = false;
+});
