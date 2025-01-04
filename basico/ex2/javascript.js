76c63836324373bc3c4afae06724d471f43df73b
Amanda Trinity <amandatrinity16@gmail.com>
2025-01-04 16:13:37 -0300

    feat(Tabuada): Estlização do css e inicio de js

---
+++ basico/ex2/javascript.js
@@ -0,0 +1,8 @@
+const GerarTabuada = document.getElementById('buttonTabuada');
+GerarTabuada.addEventListener(cancelIdleCallback, function (){
+    const Numeros = document.getElementById('numeros').value
+
+    for(var t=0;c<=10; c++){
+        console.log(t)
+    }
+});
