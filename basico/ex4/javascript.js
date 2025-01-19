ab4b8bad9aa7ee44a4e4dc7198e080a7cd109075
Amanda Trinity <amandatrinity16@gmail.com>
2025-01-19 18:03:46 -0300

    Construindo lógica js

--- basico/ex4/javascript.js
+++ basico/ex4/javascript.js
@@ -1,2 +1,15 @@
-let num = []
-console.log(num)
+/*const acrescentarItem = document.querySelector('#acrescentar-item')
+acrescentarItem.addEventListener('keypress',function(){
+    if(Event.key === 'Enter') {
+        let item = document.createElement('label')
+        let input = document.createElement('input')
+        let div = document.createElement('div')
+        let inputButton = document.createElement('input')
+        input.appendChild(item)
+        div.appendChild(item)
+        inputButton.appendChild(item)
+        div.textContent = `Olá`
+    }   
+    
+   
+})*/
