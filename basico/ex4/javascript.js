44bdcc087e8e73851c560985eaf404a9c9335ece
Amanda Trinity <amandatrinity16@gmail.com>
2025-01-22 23:53:17 -0300

    feat(To-do): Salvando os dados no localStorage

--- basico/ex4/javascript.js
+++ basico/ex4/javascript.js
@@ -14,10 +14,15 @@ acrescentarItem.addEventListener('keypress',function(){
    
 })*/
 
-let bancoDados = [ //array contendo objetos-simular um banco de dados
-    {'tarefa': 'Estudar', 'status': ''},
-   
-]
+/*let bancoDados = [ //array contendo objetos-simular um banco de dados
+   {'tarefa': 'Estudar', 'status': ''},
+]*/
+
+
+
+const getBanco = () => JSON.parse(localStorage.getItem('todoList')) ??  [];//O "??" é se esse campo for vazio,passa para ele o proximo(um array vazio)
+const setBanco = (bancoDados) => localStorage.setItem('todoList', JSON.stringify(bancoDados));
+
 const acrescentarItem = (tarefa,status='',indice) => {
     const itemLista = document.createElement('label');
     itemLista.classList.add('to-do-item')//criar classe 
@@ -46,7 +51,8 @@ A propriedade lastChild retorna o último filho do elemento*/
 }
 
 const atualizartela = () => {
-     limparTarefas()
+      limparTarefas();
+      const bancoDados= getBanco();
       bancoDados. forEach((itemLista,indice)=> acrescentarItem(itemLista.tarefa,itemLista.status,indice))
      //forEach é um método que percorre todo array e passa alguns elementos para o callBack
 }/*array.forEach(function(elemento, indice, array) {
@@ -63,24 +69,28 @@ Para cada elemento, ele executa a função fornecida como argumento, passando o 
 const inserirItem = (evento) => {
     const tecla = evento.key;
     if (tecla ==='Enter'){
+      const bancoDados = getBanco();
       bancoDados.push({ //O método push é usado em arrays para adicionar um ou mais elementos ao final do array. Ele altera o array original e retorna o novo comprimento do array após a adição.
         'tarefa': evento.target.value,// Ele representa o elemento que disparou o evento.
          'status': evento.target.value
-       
       })
+      setBanco(bancoDados)
       atualizartela(); 
       evento.target.value = '' //limpar
     }
 }
 
 const removeritem = (indice) => {
+    const bancoDados = getBanco();
     bancoDados.splice(indice,1)
+    setBanco(bancoDados);
     atualizartela()
 }
 const atualizarItem = (indice) =>{
-    bancoDados[indice].status = bancoDados[indice].status === '' ? 'checked' : ''
+    const bancoDados = getBanco();
+    bancoDados[indice].status = bancoDados[indice].status === '' ? 'checked' : '';
+    setBanco(bancoDados);
     atualizartela()
-    console.log(bancoDados)
 }
 const clickitem = (evento) => {
     const elemento = evento.target;
